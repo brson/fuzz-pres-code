5217a030c1b28a2f62851d4bcb65f0e709bfd58e
Brian Anderson <andersrb@gmail.com>
2023-08-29 12:56:28 -0600

    .

--- comrak-example/fuzz/fuzz_targets/fuzz_target_1.rs
+++ comrak-example/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,8 +1,14 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use comrak;
+use comrak::{self, Arena, ComrakOptions};
 
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
+fuzz_target!(|input: &[u8]| {
+    if let Ok(input) = std::str::from_utf8(input) {
+        let opts = &ComrakOptions::default();
+        let arena = &Arena::new();
+        let doc = comrak::parse_document(arena, input, opts);
+        let mut md = vec![];
+        comrak::format_commonmark(doc, opts, &mut md);
+    }
 });
